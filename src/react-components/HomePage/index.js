f6914b0e52a73fb393ac331474f1a70f4c38fd22
m-elhoussieny <adammohamed457@yahoo.com>
2022-06-29 19:27:27 +0200

    create HomePage and Poduct List & Item

--- src/react-components/HomePage/index.js
+++ src/react-components/HomePage/index.js
@@ -1,8 +1,37 @@
 import React from 'react';
+import ProductList from '../Product/ProductList';
 
 class HomePage extends React.Component{
     constructor(){
         super();
+        this.state={
+            productList:[
+                {
+                    id:1,
+                    name:'Codecademy',
+                    link:'http://codecademy.com',
+                    media:'/img/id.png',
+                    upvote:169,
+                    desciption:"Code for anyone",
+                    maker:{
+                        name:'hieu',
+                        avatar:'/img/f.png'
+                    }
+                },
+                {
+                    id:2,
+                    name:'Code4startup',
+                    link:'http://code4startup.com',
+                    media:'/img/id.png',
+                    upvote:280,
+                    desciption:"Code for startups",
+                    maker:{
+                        name:'leo',
+                        avatar:'/img/f.png'
+                    }
+                }
+            ]
+        }
     }
 
     render(){
@@ -13,9 +42,15 @@ class HomePage extends React.Component{
                 </header>
 
                 <section>
-                    <section className='container'>
-                        <h2>ProductList</h2>
-                    </section>
+                   <section className='container'>
+                         {
+                        this.state.productList
+                        ? 
+                        <ProductList productList={this.state.productList}/>
+                        : 
+                        null
+                        }
+                   </section>
                 </section>
             </section>
         );
